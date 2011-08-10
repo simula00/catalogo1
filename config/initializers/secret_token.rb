4026bc9d98b1c6685eca310f0d1ca4d95fb25b29
simula00 <simula00@gmail.com>
2011-08-10 17:07:21 +0200

    a

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Catalogo1::Application.config.secret_token = 'd6365168a0761652c91b9177ffa6e3cb8012b1dc7c703a3dcfdbb6564c79541cbd0c37eb6f03c651ea0eecd9cdbd9492e4af186c97f310b7bcc0750cbb49ef4a'
